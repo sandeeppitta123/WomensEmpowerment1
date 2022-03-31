986829659ab14ba66a3f98592de6327046459da3
sandeeppitta123 <sandeeppitta123@gmail.com>
2022-03-31 15:03:17 +0530

    commit message

---
+++ src/main/java/com/lti/WomensempowermentApplication.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomensempowermentApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(WomensempowermentApplication.class, args);
+	}
+
+}
